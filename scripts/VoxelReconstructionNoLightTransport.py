e47649ec09c21abceec135e0c31b226424981e7c
lichangkun2003 <89449285+lichangkun2003@users.noreply.github.com>
2026-09-14 13:57:22 +0800

    feat: add alpha supervision

--- scripts/VoxelReconstructionNoLightTransport.py
+++ scripts/VoxelReconstructionNoLightTransport.py
@@ -29,7 +29,8 @@ def render_graph_Pass():
 
 
     g.markOutput("VoxelReconstruction.color")
-    g.markOutput("AccumulatePass.output")
+    g.markOutput("VoxelReconstruction.AccuColor")
+    #g.markOutput("AccumulatePass.output")
     g.markOutput("VoxelReconstruction.dummy")
     #g.markOutput("VoxelReconstruction.lossVis")
     return g
